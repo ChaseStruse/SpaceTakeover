8554ae4a2c57f7970d98b8dd2607a5d4d3da214a
Chase <chasestruse@gmail.com>
2020-09-25 22:07:37 -0500

    Began making loop

--- SpaceTakeover/SpaceTakeover.Data/Program.cs
+++ SpaceTakeover/SpaceTakeover.Data/Program.cs
@@ -1,4 +1,5 @@
-﻿using SpaceTakeover.Data.Services;
+﻿using SpaceTakeover.Data.Models;
+using SpaceTakeover.Data.Services;
 using SpaceTakeover.Models;
 using System;
 
@@ -6,13 +7,19 @@ namespace SpaceTakeover
 {
     class Program
     {
-        private PlayerService playerService = new PlayerService();
-
         static void Main(string[] args)
         {
           
             // Game Loop
             Player player = new Player();
+            
+            Resource gold = new Resource();
+            gold.name = "Gold";
+            gold.strength = 10;
+            gold.quantity = 100;
+            gold.value = 10.5m;
+
+            ResourceService resourceService = new ResourceService();
             Console.WriteLine("Please enter Character Name: ");
             player.name = Console.ReadLine();
             bool playing = true;
@@ -28,9 +35,19 @@ namespace SpaceTakeover
                     Console.WriteLine("1 - Go Mining \n" +
                                       "2 - Go Hunting \n" +
                                       "3 - Exercise \n" +
-                                      "9 - View Player Stats and Inventory"
+                                      "4 - View Player Stats and Inventory \n" +
+                                      "9 - Exit \n"
                                       );
-                   
+                    var playerChoice = Console.ReadLine();
+
+                    Console.WriteLine("How long would you like to do the activity? 1 - 8 hours");
+                    var hours = Console.ReadLine();
+
+                    if (playerChoice == "1") resourceService.mineResource(gold, player);
+                    else if (playerChoice == "2") Console.WriteLine("Not implemented");
+                    else if (playerChoice == "3") Console.WriteLine("Not implemented");
+                    else if (playerChoice == "4") Console.WriteLine("Not implemented");
+                    else if (playerChoice == "9") isAwake = false;
                 }
             }
 
